79341599cfe380ab683658c1066514a87dfe0ce8
Jamenson Ferreira Espindula de Almeida Melo <jafesp@gmail.com>
2025-05-20 14:47:07 -0300

    Updates to the 'git-version.sh' file (BLFS ongoing release)

--- git-version.sh
+++ git-version.sh
@@ -24,8 +24,10 @@ if ! git status > /dev/null; then
     exit 0
 fi
 
-export LC_ALL=en_US.utf8
-export TZ=America/Chicago
+#export LC_ALL=en_US.utf8
+#export TZ=America/Chicago
+export LC_ALL=pt_BR.utf8
+export TZ=America/Recife
 
 commit_date=$(git show -s --format=format:"%cd" --date=local)
 short_date=$(date --date "$commit_date" "+%Y-%m-%d")
@@ -42,7 +44,7 @@ case $day in
     * ) suffix="th";;
 esac
 
-full_date="$month $day$suffix, $year"
+full_date="$day de $month de $year"
 
 sha="$(git describe --abbrev=1)"
 version=$(echo "$sha" | sed 's/-g[^-]*$//')
